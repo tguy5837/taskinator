45f264c56c9850e6178aa219ca1e756f8e6cdc81
DTGuy <tguy5837@gmail.com>
2022-08-16 15:21:52 -0400

    add functioning delete button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,8 @@
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
+var pageContentEl = document.querySelector("#page-content")
+
 var taskIdCounter = 0;
 
 var taskFormHandler = function (event) {
@@ -93,3 +95,26 @@ var createTaskEl = function (taskDataObj) {
 }
 
 formEl.addEventListener("submit", taskFormHandler);
+
+var taskButtonHandler = function (event) {
+    console.log(event.target);
+
+    // if (event.target.matches(".delete-btn")) {
+    //     console.log("you clicked the delete button");
+
+    //     var taskId = event.target.getAttribute("data-task-id");
+    //     console.log(taskId);
+    // }
+
+    if (event.target.matches(".delete-btn")) {
+        var taskId = event.target.getAttribute("data-task-id");
+        deleteTask(taskId);
+    }
+};
+
+var deleteTask = function (taskId) {
+    var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
+    taskSelected.remove();
+};
+
+pageContentEl.addEventListener("click", taskButtonHandler);
